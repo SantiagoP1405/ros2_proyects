bf02fab82a214534aaa4c3982f5d5f2a2f970616
Santiago Gómez <santipatito04@gmail.com>
2025-08-05 22:55:23 -0600

    Migration to Ubunu 22.04 Humble - Migración a la distribución de humble para ROS2. - Cambios realizados especificamente para cambiar a la versión Ingition Fortress de Gazebo, así como de las direcciones de los elementos del mundo.

--- differential_robot_bringup/launch/gz_sim.launch.py
+++ differential_robot_bringup/launch/gz_sim.launch.py
@@ -8,7 +8,7 @@ import os
 from ament_index_python.packages import get_package_share_path, get_package_share_directory # type: ignore
 
 def generate_launch_description():
-    gz_launch_path = os.path.join(get_package_share_directory('ros_gz_sim'), 'launch')
+    gz_launch_path = os.path.join(get_package_share_directory('ros_ign_gazebo'), 'launch')
     
     urdf_path = os.path.join(get_package_share_path('differential_robot_description'),
         'urdf',
@@ -51,12 +51,12 @@ def generate_launch_description():
     gz_sim_launch_path = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             gz_launch_path,
-            "/gz_sim.launch.py"
+            "/ign_gazebo.launch.py"
         ]), launch_arguments={'gz_args' : f'{gz_world_path} -r'}.items()
     )
 
     spawn_robot_node = Node(
-        package="ros_gz_sim",
+        package="ros_ign_gazebo",
         executable="create",
         arguments=['-topic', '/robot_description']
     )
