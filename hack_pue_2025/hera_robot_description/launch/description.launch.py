e9544b26b8dbf5c25ae094c1dd98f62c5226e5ac
Santiago Gómez <santipatito04@gmail.com>
2025-08-16 16:42:24 -0600

    RVIZ LAUNCH
    - Archivo de lanzamiento para Rviz y configuración inicial de Rviz

---
+++ hack_pue_2025/hera_robot_description/launch/description.launch.py
@@ -0,0 +1,42 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node # type: ignore
+from launch_ros.descriptions import ParameterValue # type: ignore
+from launch.substitutions import Command # type: ignore
+import os
+from ament_index_python.packages import get_package_share_path # type: ignore
+
+def generate_launch_description():
+    
+    urdf_path = os.path.join(get_package_share_path('hera_robot_description'),
+        'urdf',
+        'diff_hera.urdf.xacro'
+    )
+
+    rviz_path = os.path.join(get_package_share_path('hera_robot_description'),
+        'rviz',
+        'config.rviz'
+    )
+    
+    robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{'robot_description': robot_description}],
+    )
+
+    joint_state_publisher_gui_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui"
+    )
+
+    rviz2_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=['-d', rviz_path],
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        joint_state_publisher_gui_node,
+        rviz2_node
+    ])
