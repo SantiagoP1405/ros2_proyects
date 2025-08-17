32f5e70c21182077f41226b82f81ffa7daea87ad
Santiago Gómez <santipatito04@gmail.com>
2025-08-16 19:54:23 -0600

    H.E.R.A IN CLASSROOM
    - Mundo actualizado para mejor desempeño
    - Launch actualizado con el nuevo mundo
    - Configuración actualizada para el laser scan (servirá para posteriormente mapear el mundo)

--- hack_pue_2025/hera_robot_bringup/launch/gz_hera.launch.py
+++ hack_pue_2025/hera_robot_bringup/launch/gz_hera.launch.py
@@ -23,6 +23,10 @@ def generate_launch_description():
     gz_bridge_config_path = os.path.join(get_package_share_path('hera_robot_bringup'),
         'config',
         'gz_bridge.yaml')
+    
+    gz_world_path = os.path.join(get_package_share_path('hera_robot_bringup'),
+        'worlds',
+        'classroom.sdf')
 
     robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
     robot_state_publisher_node = Node(
@@ -35,13 +39,20 @@ def generate_launch_description():
         PythonLaunchDescriptionSource([
             gz_launch_path,
             "/ign_gazebo.launch.py"
-        ]), launch_arguments={'gz_args' : 'empty.sdf -r'}.items()
+        ]), launch_arguments={'gz_args' : f'{gz_world_path} -r'}.items()
     )
 
     spawn_robot_node = Node(
         package="ros_ign_gazebo",
         executable="create",
-        arguments=['-topic', '/robot_description']
+        arguments=[
+            '-topic', '/robot_description',
+            '-name', 'diff_hera',
+            '-x', '-2.420820',
+            '-y', '-5.430130',
+            '-z', '0.0',
+            '-Y', '1.57'
+    ]
     )
 
     rviz2_node = Node(
