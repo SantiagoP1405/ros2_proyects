64f0396f20d194fe0561de25b19bf5888e264536
Santiago Gómez <santipatito04@gmail.com>
2025-08-24 20:04:25 -0600

    GREEN HOUSE ROBOT - Paquetes creados con ament cmake para robot diferencial de prueba para la navegación dentro de un invernadero. - Mapeo realizado de un mundo prototipo de invernadero

---
+++ greenhouse_robot/greenhouse_robot_bringup/launch/gz_ghbot.launch.py
@@ -0,0 +1,105 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node # type: ignore
+from launch.actions import IncludeLaunchDescription #type: ignore
+from launch.launch_description_sources import PythonLaunchDescriptionSource # type: ignore
+from launch_ros.descriptions import ParameterValue # type: ignore
+from launch.substitutions import Command # type: ignore
+import os
+from ament_index_python.packages import get_package_share_path, get_package_share_directory # type: ignore
+
+def generate_launch_description():
+    gz_launch_path = os.path.join(get_package_share_directory('ros_ign_gazebo'), 'launch')
+    
+    urdf_path = os.path.join(get_package_share_path('greenhouse_robot_description'),
+        'urdf',
+        'greenhouse_bot.urdf.xacro'
+    )
+
+    rviz_path = os.path.join(get_package_share_path('greenhouse_robot_description'),
+        'rviz',
+        'config.rviz'
+    )
+
+    gz_bridge_config_path = os.path.join(get_package_share_path('greenhouse_robot_bringup'),
+        'config',
+        'gz_bridge.yaml')
+
+    gz_world_path = os.path.join(get_package_share_path('greenhouse_robot_bringup'),
+         'worlds',
+         'greenhouse_bell.sdf')
+    
+    # nav2_bringup_launch_path = os.path.join(
+    #     get_package_share_directory('nav2_bringup'),
+    #     'launch',
+    #     'bringup_launch.py'
+    # )
+
+    # map_path = os.path.join(
+    #     get_package_share_directory('greenhouse_robot_bringup'),
+    #     'maps',
+    #     'my_map_1.yaml'
+    # )
+
+
+    robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{'robot_description': robot_description}],
+    )
+
+    gz_sim_launch_path = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            gz_launch_path,
+            "/ign_gazebo.launch.py"
+        ]), launch_arguments={'gz_args' : f'{gz_world_path} -r'}.items()
+    )
+
+    # gz_sim_launch_path = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource([
+    #         gz_launch_path,
+    #         "/ign_gazebo.launch.py"
+    #     ]), launch_arguments={'gz_args' : 'empty.sdf -r'}.items()
+    # )
+
+    spawn_robot_node = Node(
+        package="ros_ign_gazebo",
+        executable="create",
+        arguments=['-topic', '/robot_description']
+    )
+
+    rviz2_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=['-d', rviz_path],
+        parameters=[{'use_sim_time': True}]
+    )
+
+    gz_bridge_node = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        parameters=[{'config_file' : gz_bridge_config_path}]
+    )
+
+    # nav2_test_node = Node(
+    #     package="greenhouse_robot_navigation",
+    #     executable="nav2_test"
+    # )
+    
+    # nav2_bringup_launch = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(nav2_bringup_launch_path),
+    #     launch_arguments={
+    #         'use_sim_time': 'True',
+    #         'map': map_path
+    #     }.items()
+    # )
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        gz_sim_launch_path,
+        spawn_robot_node,
+        gz_bridge_node,
+        #nav2_test_node,
+        rviz2_node,
+        #nav2_bringup_launch
+    ])
